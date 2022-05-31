ce4aab6a2f0f6fd1be6990553ef11dd3bfdf40c9
Nikita Kovalevskiy <koval701@gmail.com>
2022-05-31 23:44:59 +0700

    done

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from app.api.router import main_router
 from app.core.config import settings
-from app.core.init_db import create_first_superuser
+# from app.core.init_db import create_first_superuser
 
 app = FastAPI(title=settings.app_title, description=settings.description)
 app.include_router(main_router)
